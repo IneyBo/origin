572fb610346d4c827b836981c5d13071e919fcf8
Aleksandr Pshikin <apshikin@internet.ru>
2022-09-12 20:31:11 +0300

    add condition if size array is zero - task_1.cpp

--- Lesson_1/Task_1/Task_1.cpp
+++ Lesson_1/Task_1/Task_1.cpp
@@ -4,11 +4,19 @@
 int* createArr(std::ifstream& fin, int* size) // Create array from file
 {
     fin >> *size; // Removed array size from array to separate variable
+
     int* arr = new int[*size];
 
-    for (int i = 0; i < *size; i++)
+    if (*size > 0)
     {
-        fin >> arr[i];
+        for (int i = 0; i < *size; i++)
+        {
+            fin >> arr[i];
+        }
+    }
+    else if (*size == 0) // Condition if size is zero
+    {
+        arr = nullptr;
     }
 
     return arr;
@@ -36,23 +44,38 @@ int main()
         std::ofstream fout(fileOut);
 
         // Second array output
-        fout << sizeArr2 << std::endl; // Number of array elements output
-        fout << arr2[sizeArr2 - 1]; // Last element of array output
-        for (int i = 0; i < sizeArr2 - 1; i++) // Other elements output
+        if (sizeArr2 > 0)
         {
-            fout << " " << arr2[i];
+            fout << sizeArr2 << std::endl;
+            fout << arr2[sizeArr2 - 1];
+            for (int i = 0; i < sizeArr2 - 1; i++)
+            {
+                fout << " " << arr2[i];
+            }
+        }
+        else if (sizeArr2 == 0) // Condition if size is zero
+        {
+            fout << sizeArr2;
         }
         fout << std::endl;
 
-        delete[] arr2; // Cleaned up memory for two arrays separately
+        delete[] arr2;
 
         // First array output
-        fout << sizeArr1 << std::endl; // Number of array elements output
-        for (int i = 1; i < sizeArr1; i++) // Output of array elements from the second to the last
+        if (sizeArr1 > 0)
         {
-            fout << arr1[i] << " ";
+            fout << sizeArr1 << std::endl;
+            for (int i = 1; i < sizeArr1; i++)
+            {
+                fout << arr1[i] << " ";
+            }
+            fout << arr1[0];
         }
-        fout << arr1[0]; // First element output
+        else if (sizeArr1 == 0) // Condition if size is zero
+        {
+            fout << sizeArr1;
+        }
+        fout << std::endl;
 
         delete[] arr1;
 
