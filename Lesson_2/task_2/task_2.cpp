80993bddd3065e67e02304f06d71df50e3c47659
Aleksandr Pshikin <apshikin@internet.ru>
2022-09-14 22:01:23 +0300

    add to Lesson_2 -> task_2.cpp, task_3.cpp

---
+++ Lesson_2/task_2/task_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+struct BankAccount
+{
+    unsigned long long m_numberOfAccount;
+    std::string m_name;
+    float m_amountMoney;
+};
+
+void changeAmountMoney (BankAccount& account, float amountMoney)
+{
+    account.m_amountMoney = amountMoney;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    BankAccount account;
+    std::cout << "Введите номер счета: ";
+    std::cin >> account.m_numberOfAccount;
+    std::cout << "Введите Имя владельца: ";
+    std::cin >> account.m_name;
+    std::cout << "Введите баланс: ";
+    std::cin >> account.m_amountMoney;
+
+    float amountMoney = 0;
+
+    std::cout << "Введите новый баланс: ";
+    std::cin >> amountMoney;
+
+    changeAmountMoney(account, amountMoney);
+
+    std::cout << "Ваш счет: " << account.m_name << ", " << account.m_numberOfAccount << ", " << account.m_amountMoney;
+
+    return 0;
+}
---
+++ Lesson_2/task_3/task_3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct Address
+{
+    std::string m_country;
+    std::string m_city;
+    std::string m_street;
+    unsigned short m_house;
+    unsigned short m_flat;
+    unsigned long m_index;
+};
+
+void printAddress(Address& addrress)
+{
+    std::cout << "Страна: " << addrress.m_country << std::endl;
+    std::cout << "Город: " << addrress.m_city << std::endl;
+    std::cout << "Улица: " << addrress.m_street << std::endl;
+    std::cout << "Номер дома: " << addrress.m_house << std::endl;
+    std::cout << "Номер квартиры: " << addrress.m_flat << std::endl;
+    std::cout << "Индекс: " << addrress.m_index << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Address address1 =
+    {
+        "Россия",
+        "Москва",
+        "Арбат",
+        12,
+        8,
+        123456
+    };
+
+    Address address2 =
+    {
+        "Россия",
+        "Ижевск",
+        "Пушкина",
+        59,
+        143,
+        953769
+    };
+
+    printAddress(address1);
+    std::cout << std::endl;
+    printAddress(address2);
+
+    return 0;
+}
