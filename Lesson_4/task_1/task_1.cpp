0fb04457ac0488a7a5cf791b0cb5a366929b9896
Aleksandr Pshikin <apshikin@internet.ru>
2022-09-20 15:17:01 +0300

    Add Lesson_4/task_1

---
+++ Lesson_4/task_1/task_1.cpp
@@ -0,0 +1,107 @@
+﻿#include <windows.h>
+
+#include <iostream>
+#include <fstream>
+
+class Address
+{
+private:
+    std::string city;
+    std::string street;
+    unsigned short house;
+    unsigned short flat;
+public:
+    Address() // Конструктор без параметров
+    {
+        city = "неизвестно";
+        street = "неизвестно";
+        house = 0;
+        flat = 0;
+    }
+
+    void setAddress(std::ifstream& value) // Сеттер
+    {
+        value >> city;
+        value >> street;
+        value >> house;
+        value >> flat;
+    }
+
+    // Геттеры
+    std::string getCity()
+    {
+        return city;
+    }
+    std::string getStreet()
+    {
+        return street;
+    }
+    unsigned short getHouse()
+    {
+        return house;
+    }
+    unsigned short getFlat()
+    {
+        return flat;
+    }
+};
+
+class AddressConverter // Класс для чтения и записи адресов
+{
+private:
+    int size = 0;
+    Address* arrOfAddresses = nullptr;
+public:
+    void fillArrOfAddresses(std::ifstream& fin) // Заполнение массива адресов
+    {
+        fin >> size;
+        if (size != 0)
+        {
+            arrOfAddresses = new Address[size];
+            for (int i = 0; i < size; ++i)
+            {
+                arrOfAddresses[i].setAddress(fin);
+            }
+        }
+    }
+    void outArrOfAddresses(std::ofstream& fout) // Вывод в файл массива адресов
+    {
+        fout << size;
+        if (size != 0)
+        {
+            for (int i = size - 1; i >= 0 ; --i)
+            {
+                fout << std::endl << arrOfAddresses[i].getCity() << ", " << arrOfAddresses[i].getStreet() << ", " << arrOfAddresses[i].getHouse() << ", " << arrOfAddresses[i].getFlat();
+            }
+            delete[] arrOfAddresses;
+        }
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string fileIn = "in.txt", fileOut = "out.txt";
+    std::ifstream fin(fileIn);
+
+    if (!fin.is_open())
+    {
+        std::cout << "Не удалось открыть файл" << std::endl;
+    }
+    else
+    {
+        AddressConverter setOfAddresses;
+        setOfAddresses.fillArrOfAddresses(fin);
+
+        fin.close();
+
+        std::ofstream fout(fileOut);
+
+        setOfAddresses.outArrOfAddresses(fout);
+
+        fout.close();
+    }
+    return 0;
+}
