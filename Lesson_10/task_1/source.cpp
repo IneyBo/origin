8596356475c174eeb1ef1e1eef7e2f6f1f23c4e0
Aleksandr Pshikin <apshikin@internet.ru>
2022-10-24 20:19:11 +0300

    Add Lesson_10 -> task_1, task_2

---
+++ Lesson_10/task_1/source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+#include <Windows.h>
+
+int main(int argc, char** argv)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите имя: ";
+
+	std::string name = "_";
+
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name;
+
+	return 0;
+}
